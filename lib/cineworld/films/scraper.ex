3af088b185eb96fdb1a459168545c32e0abc2816
Matt Weldon <matt@mattweldon.com>
2017-03-21 22:12:10 +0000

    Feature (film-scraper): Allow data to be scraped when given a URL

--- lib/cineworld/films/scraper.ex
+++ lib/cineworld/films/scraper.ex
@@ -2,7 +2,10 @@ defmodule Cineworld.Films.Scraper do
   @moduledoc """
   Scrape the Cineworld website for Film information
   """
-  
+
+  def load_page(film_url) do
+    %{} |> Map.put(:html, HTTPoison.get!(film_url, []).body)
+  end
   def load_page(cinema_id, edi) do
     film = Cineworld.Films.get(cinema_id, edi)
     %{} |> Map.put(:html, HTTPoison.get!(film[:film_url], []).body)
